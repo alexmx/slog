b7b3e0b256d3c475ff5d274f11947e9696e1ae13
Alex Maimescu <maimescu.alex@gmail.com>
2026-02-17 20:25:00 +0000

    Add doctor command to check system requirements

--- Sources/slog/Commands/RootCommand.swift
+++ Sources/slog/Commands/RootCommand.swift
@@ -125,7 +125,7 @@ struct Slog: AsyncParsableCommand {
           slog list simulators --booted
         """,
         version: slogVersion,
-        subcommands: [StreamCommand.self, ShowCommand.self, ListCommand.self, ProfileCommand.self],
+        subcommands: [StreamCommand.self, ShowCommand.self, ListCommand.self, ProfileCommand.self, DoctorCommand.self],
         defaultSubcommand: StreamCommand.self
     )
 }
